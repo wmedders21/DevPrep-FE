b8d1fb6ebb91df1914355dab5b95a00471625937
Igor Decess <i.decess@comcast.net>
2022-07-14 17:03:11 -0600

    Create commented basic structure for cypress testing login page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,41 @@
-describe('empty spec', () => {
+describe('Login Page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000/')
+    //intercept GET here with fixture
   })
 
   it('should land on login', () => {
     expect(true).to.equal(true)
   })
+
+//should contain title
+
+
+//should contain checkmarks
+
+
+//should contain text to log in
+
+
+//should contain two input fields and check inputs
+
+
+//should contain login button
+
+
+//should contain sign up request text which leads to modal
+
+
+//should be able to log in with username and email filled
+
+
+//should NOT be able to click login button if email is not filled
+
+
+//should NOT be able to click login button if username is not filled
+
+
+//should be able to display an error message if login is unsuccessful
+
+
 })
