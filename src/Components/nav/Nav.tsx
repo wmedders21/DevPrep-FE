dc6bd1292f2b3f88eb2d9ee1544f5334d5c47a9c
Rio Foster <friotious@gmail.com>
2022-07-13 20:57:50 -0700

    Update link path

--- src/Components/nav/Nav.tsx
+++ src/Components/nav/Nav.tsx
@@ -3,9 +3,9 @@ import "./Nav.scss"
 import { NavLink, Link, Navigate } from 'react-router-dom'
 import Select from 'react-select';
 const Decks = [
-    { label: <NavLink className='nav-link dd-menu-be' to='/technicalBE'><p>FE</p></NavLink>},
-    { label: <NavLink className='nav-link dd-menu-fe' to='/technicalBE'>BE</NavLink> },
-    { label: <NavLink className='nav-link dd-menu-behavioral' to='/behavioral'><p>Behavioral</p></NavLink> }
+    { label: <Link className='nav-link dd-menu-be' to='/flashcards/technicalBE'>FE</Link>},
+    { label: <NavLink className='nav-link dd-menu-fe' to='/flashcards/technicalFE'>BE</NavLink> },
+    { label: <NavLink className='nav-link dd-menu-behavioral' to='/flashcards/behavioral'>Behavioral</NavLink> }
 ]
 
 
