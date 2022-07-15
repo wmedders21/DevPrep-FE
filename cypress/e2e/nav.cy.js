6a924f6b160ee8c341f1980742fc0b4b5566dddb
Rio Foster <friotious@gmail.com>
2022-07-15 09:26:22 -0700

    Update cy nav dropdown menu test

--- cypress/e2e/nav.cy.js
+++ cypress/e2e/nav.cy.js
@@ -1,9 +1,30 @@
 describe('empty spec', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:3000')
+      cy.visit('http://localhost:3000/dashboard')
     })
-  
-    it('should land on login', () => {
-      expect(true).to.equal(true)
+
+    it('User should see several navigation options, home, signout, decks', () => {
+        cy.get('.nav').contains('DEVPREP')
+        .get('.nav').contains('Home')
+        .get('.nav').contains('Decks')
+        .get('.nav').contains('Logout')
+        
     })
-  })
+
+    it('User should be able to navigate to different decks with dropdown menu', () => {
+        cy.get('.deck-select').contains('Decks')
+        cy.get('.deck-select').click()
+        cy.get('.dd-menu').contains('Technical Back End')
+        cy.get('.dd-menu-be').click()
+        .url().should('eq', 'http://localhost:3000/flashcards/technicalBE')
+        cy.get('.deck-select').click()
+        cy.get('.dd-menu-behavioral').click()
+        .url().should('eq', 'http://localhost:3000/flashcards/behavioral')
+    })
+
+      it('User should be able to signout, which will take them to login page', () => {
+        cy.url().should('eq', 'http://localhost:3000/')
+        cy.get('.signout-button').click()
+
+      })
+    })
