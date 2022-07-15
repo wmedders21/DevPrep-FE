5b3d629162417011580cc4813adf1ca48f15d1ea
Igor Decess <i.decess@comcast.net>
2022-07-14 23:18:01 -0600

    Create commented basic structure for cypress testing for modal

---
+++ cypress/e2e/modal.cy.js
@@ -0,0 +1,25 @@
+describe('Modal', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    //intercept GET here with fixture
+  })
+
+//should be able to see modal's title
+
+
+//should be able to input 2 fields and check those inputs
+
+
+//should be able to see sign up botton
+
+
+//should be able to see close botton
+
+
+//should be able to sign up with username and email upon clicking signup button
+
+
+//should be able to display an error message if signup is unsuccessful?
+
+
+})
