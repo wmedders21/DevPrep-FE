b6de9202a696f1cd6275a2fe213c3eb45e0bdc2a
Rio Foster <friotious@gmail.com>
2022-07-18 16:50:16 -0700

    Remove default modal styling

--- src/Components/flashcardPage /components/CreateNewFlashcardButton.tsx
+++ src/Components/flashcardPage /components/CreateNewFlashcardButton.tsx
@@ -1,25 +1,13 @@
 import * as React from 'react';
+import './CreateNewFlashcardButton.scss'
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import Rating from '@mui/material/Rating';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { TextField } from '@mui/material';
 
 type Props = {}
-
-const style = {
-	position: 'absolute' as 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: '35vw',
-	height: '80vh',
-	bgcolor: 'background.paper',
-	border: '2px solid #000',
-	boxShadow: 24,
-	p: 4,
-  };
+ const theme = createTheme({})
 
 	export default function CreateNewFlashcardButton({}: Props) {
 		const [open, setOpen] = React.useState(false);
@@ -32,6 +20,7 @@ const style = {
 		  const createNewCard = () => {
 			// new card POST 
 		  }
+
 		return (
 		  <div>
 			<Button onClick={handleOpen}>Create New</Button>
@@ -41,15 +30,16 @@ const style = {
 			  aria-labelledby="modal-modal-title"
 			  aria-describedby="modal-modal-description"
 			>
-			  <Box sx={style}>
+			  <Box component='form'>
 
-				<Rating />
-				<h4>Create a new Card</h4>
-				<form onSubmit={() => createNewCard()}>
-					
-            		<TextField						
+				<h3>Create a new Card</h3>
+					<ThemeProvider theme={theme}>
+            		<TextField
+						color='secondary'						
+						required
+						variant='standard'
 						multiline
-						rows={5}
+						rows={6}
 						type='text'
 						label='Question'
 						value={question}
@@ -59,15 +49,17 @@ const style = {
 			
 					<TextField						
 						multiline
-						rows={5}
+						color='secondary'						
+						variant='standard'
+						rows={6}
 						type='text'
 						label='Answer'
 						value={answer}
 						className='newcard-textfield-answer'
 						onChange={e => setAnswer(e.target.value)}
 					/>
-					<input type='submit' value='Create Card' className='newcard-input-submit-btn'/>
-				</form>
+					<Button color='primary' variant='contained' onSubmit={() => createNewCard()} >Create New Card</Button>
+					</ThemeProvider>
 			  </Box>
 			</Modal>
 		  </div>
