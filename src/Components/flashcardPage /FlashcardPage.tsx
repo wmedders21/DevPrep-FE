58ed1a62dc8f7ea07adabf882d62611187a7e94e
Michael Putnam <mikeputnam713@gmail.com>
2022-07-18 05:36:25 -1000

    add initial flashcard components

--- src/Components/flashcardPage /FlashcardPage.tsx
+++ src/Components/flashcardPage /FlashcardPage.tsx
@@ -6,36 +6,21 @@ import Decks from "../decks/Decks";
 import FlashcardCarousel from "./flashcardCarousel/FlashcardCarousel";
 import FlashcardList from "./flashcardList/FlashcardList";
 const { data } = require("../../mock-data/getUsersCards.json");
+const height = window.innerHeight
 
 const FlashcardPage = () => {
   let { id } = useParams();
-  const [deckId, setDeckId] = useState("");
-  const [deckEnum, setDeckEnum] = useState<null | number>(null);
+
   const [deck, setDeck] = useState([]);
 
   useEffect(() => {
-    setDeckId(id);
-  });
-
-  useEffect(() => {
-    switch (deckId) {
-      case "behavioral":
-        return setDeckEnum(0);
-      case "technicalFE":
-        return setDeckEnum(1);
-      case "technicalBE":
-        return setDeckEnum(2);
-      default:
-        return;
-    }
-  }, [deckId]);
-
-  useEffect(() => {
-    setDeck(data.attributes.cards.filter((card) => card.type === deckEnum));
-  }, [deckEnum]);
+    setDeck(data[id]);
+  }, []);
 
   return (
-    <div className="ççç">
+    <div className="flashcards-page" style={{
+      height: height
+    }}>
       <Nav />
       <FlashcardCarousel deck={deck} />
       <FlashcardList deck={deck}  />
--- src/Components/flashcardPage /flashcard/Card.tsx
+++ src/Components/flashcardPage /flashcard/Card.tsx
@@ -1,11 +1,37 @@
+import React from 'react'
+import './Card.scss'
 
+type Props = {
+  card: {
+    id: string
+    type: string
+    attributes: {
+      category: string
+      competenceRating: number
+      frontSide: string
+      backSide: string
+      userId: string
+    }
+  }
+}
 
-const Card = () => {
+function Card({ card }: Props) {
   return (
-    <div className="flashcard">
-      I am a Card
-    </div>
+    <div className='flashcard-container'>
+      <div className='flashcard-front'>
+        <input type='range' className='flashcard-front-rating' />
+        <h2>Question:</h2>
+        <p>Front</p>
+        <button className='toggle-flashcard-button'>Flip</button>
+        <label><span>Deck Type</span>|<span>Card Number</span></label>
+      </div>
+      <div className='flashcard-back'>
+        <p>Back</p>
+        <button className='toggle-flashcard-button'>Flip</button>
+        <label><span>Deck Type</span>|<span>Card Number</span></label>
+      </div>
+    </div> 
   )
 }
 
-export default Card;
+export default Card
