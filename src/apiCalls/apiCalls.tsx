a8561f1fa5603d93d6ccc23590ff2ea65177bb24
Rio Foster <friotious@gmail.com>
2022-07-19 11:50:32 -0700

    Add create new card api call

--- src/apiCalls/apiCalls.tsx
+++ src/apiCalls/apiCalls.tsx
@@ -1,7 +1,29 @@
-const apiCalls = {
-	getCards: () => {
-		return fetch('https://devprep-be.herokuapp.com/api/v1/users/3/cards').then(res => res.json()).catch(err => alert(err))
-	}
+type newCard = {
+	category: string,
+	frontSide: string,
+	backSide: string
 }
 
-export default apiCalls
+const getCards = () => {
+		return fetch('https://devprep-be.herokuapp.com/api/v1/users/3/cards').then(res => res.json()).catch(err => alert(err))
+	}
+
+
+const createNewCard = (newCard: newCard, userId: number) => {
+	return fetch(`https://devprep-be.herokuapp.com/api/v1/users/${userId}}/cards)`, {
+		method: "POST",
+		body: JSON.stringify(newCard),
+		headers: {
+		  "Content-Type": "application/json"
+		}
+	  }).then(res => {
+		if(res.ok) {
+		  return res.json();
+		} else {
+		  throw new Error();
+		}
+	  })
+	}
+
+export default { createNewCard, getCards }
+
