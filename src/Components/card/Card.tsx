789dff1c6d4d8592fd9c6da95a88bcefdb6a83d2
Ross Landino <mrlandino@gmail.com>
2022-07-20 15:02:07 -0600

    Add API call for quote of the day

--- src/Components/card/Card.tsx
+++ src/Components/card/Card.tsx
@@ -1,10 +1,11 @@
 import './Card.scss';
 
-const Card = () => {
+
+const Card= ({quote}: {quote: string}) => {
   return (
     <div className="quote-of-the-day">
       <h2>Quote of the Day</h2>
-      <p>"I choose a lazy person to do a hard job, because a lazy person will find an easy way to do it." — Bill Gates</p>
+      <p>{quote}</p>
     </div>
   )
 }
