72a073cf0adb0d1e3425eb5de4c4a8b96b680e62
Rio Foster <friotious@gmail.com>
2022-07-20 00:19:39 -0700

    Update apicall

--- src/Components/flashcardPage /flashcardList/FlashcardList.tsx
+++ src/Components/flashcardPage /flashcardList/FlashcardList.tsx
@@ -1,7 +1,11 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./FlashcardList.scss";
+import { useParams } from "react-router-dom";
+import { getCards } from '../../../apiCalls/apiCalls'
+
 import Select, { StylesConfig } from 'react-select';
-import {DeckContext} from '../../../Context'
+import {UserContext, CardContext, DeckContext} from "../../../Context";
+
 
 const filters = [
   { label: "stars high-low" },
@@ -26,14 +30,14 @@ const dropdownMenuStyles = {
 }
 
 const FlashcardList = () => {
-	const {deck} = useContext(DeckContext)
+	const { deck } = useContext(DeckContext)
 
 	const renderDeck = () => {
-
+		console.log(deck)
 		const deckList = deck.map(card => (
-			<div className={`decklist-card ${card.category}`} key={card.id}>
-				<p className='decklist-question'>Question: {card.question}</p>
-				<p className='decklist-response'>Response: {card.answer}</p>
+			<div className={`decklist-card decklist-category-${card.attributes.category}`} id={card.id} key={card.id}>
+				<p className='decklist-question'>Question: {card.attributes.frontSide}</p>
+				<p className='decklist-response'>Response: {card.attributes.backSide}</p>
 				<button className='remove-card-btn' name='delete' onClick={() => true}>Remove From Deck</button>
 				<button className='edit-card-btn' name='edit' onClick={() => true}>Edit</button>
 			</div>
@@ -47,7 +51,7 @@ const FlashcardList = () => {
   return (
 	<div className="flashcard-list-container">
 		<div className='deck-list-header'>
-			<h3 className='deck-list-header-deckname'>Deck Name{deck.name}</h3>
+			<h3 className='deck-list-header-deckname'>DeckName:</h3>
 			<Select 
 				className='deck-list-filter'
 				styles={dropdownMenuStyles} 
