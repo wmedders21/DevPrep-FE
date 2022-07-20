6304daf10bcb94a4582727fbea4a5e086b5af5d9
Rio Foster <friotious@gmail.com>
2022-07-20 00:18:30 -0700

    Add navigation testing

--- cypress/e2e/flashcardPage.cy.js
+++ cypress/e2e/flashcardPage.cy.js
@@ -1,3 +1,4 @@
+const { Children } = require("react");
 
 describe('FlashcardPage', () => {
     beforeEach(() => {
@@ -16,7 +17,35 @@ describe('FlashcardPage', () => {
 
   });
 
-  it('flashcard page url path should correspond wi', () => {
-    cy.url().should('eq', 'http://localhost:3000/flashcards/behavioralCards')
+  it('user should be able to navigate between decks from navbar', () => {
+    cy.get('.deck-select').click()
+    .get('.dd-menu-behavioral').click()
+    .get('.flashcard-footer').should('contain', 'behavioral')
+    .get('.deck-select').click()
+    .get('.dd-menu-fe').click()
+    .get('.flashcard-footer').should('contain', 'technicalFE')
+    .get('.deck-select').click()
+    .get('.dd-menu-be').click()
+    .get('.flashcard-footer').should('contain', 'technicalBE')
+  })
+
+  it('user should see a carousel of flashcards and be able to navigate through it', () => {
+    cy.get('.flashcard-front').contains('Question:')
+    .get('.swiper-button-next').click()
+    .get('.swiper-button-prev').click()
+    cy.get('.toggle-flashcard-button').contains('Flip To Back')
+  })
+
+  it('user should be able to create a new card, update current card, or delete current card', () => {
+    cy.get('.carousel-bottom-nav-container')
+    .should('contain', 'Create New' )
+    .should('contain', 'Update Current FlashCard' )
+    .should('contain', 'Delete' )
+  })
+
+  it('user should see a list of cards from the current deck', () => {
+    cy.get('.flashcard-list-container')
+    .should('contain')
+
   })
 })
